6d48e63039ab5f8217c684c2bc0503c398d07f23
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-04-28 23:51:35 -0500

    Update .drone/publish.sh to not use double-dollar signs
    Were previously needed when they were processed by the YAML file, but 
    they no longer are, and are thus not needed.

--- .drone/publish.sh
+++ .drone/publish.sh
@@ -7,9 +7,9 @@ apt install curl -y
 # PUBLISH PACKAGE TO REPOSITORY
 package="$(ls *.deb)"
 ## EXIT WITH CODE 1 IF MULTIPLE PACKAGES WERE LISTED IN PACKAGE DIRECTORY
-if [ "$(echo $${package[@]} | awk -F ' ' '{print $2}')" != "" ]; then exit 1; fi
+if [ "$(echo ${package[@]} | awk -F ' ' '{print $2}')" != "" ]; then exit 1; fi
 
-curl -u "system:$${nexus_pass}" \
+curl -u "system:${nexus_pass}" \
      -H "Content-Type: multipart/form-data" \
-     --data-binary "@$${package}" \
+     --data-binary "@${package}" \
      "https://nexus.hunterwittenborn.com/repository/makedeb/"
