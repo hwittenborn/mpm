0d98c4bdb5637346ba1649dbf901bc044222dcaa
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-04-28 23:36:53 -0500

    Fix issue with variable generation

--- .drone/alpha.sh
+++ .drone/alpha.sh
@@ -1,4 +1,4 @@
 source <(cat PKGBUILD | grep -E 'pkgname=|pkgver=')
 
 sed -i "0,/pkgname.*/{s/pkgname.*/pkgname=$pkgname-alpha/}" PKGBUILD
-sed -i "0,/pkgver.*/{s/pkgver.*/pkgver=$pkgver\alpha/}" PKGBUILD
+sed -i "0,/pkgver.*/{s/pkgver.*/pkgver=$pkgver'alpha'/}" PKGBUILD
