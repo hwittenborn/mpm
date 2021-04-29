8fb2dec12ae92e213130d1bebe303cbe81e694d4
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-04-29 18:43:40 -0500

    Remove alpha.sh script

--- .drone/alpha.sh
+++
@@ -1,5 +0,0 @@
-set -x
-source <(cat PKGBUILD | grep -E 'pkgname=|pkgver=')
-
-sed -i "0,/pkgname.*/{s/pkgname.*/pkgname=$pkgname-alpha/}" PKGBUILD
-sed -i "0,/pkgver.*/{s/pkgver.*/pkgver=$pkgver'alpha'/}" PKGBUILD
