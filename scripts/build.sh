18131b1016d1ff5a639bf2a093f9bdb2c6f9f115
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-05-02 02:41:32 -0500

    Drone CI testing

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,7 +13,8 @@ if [[ "${release_type}" == "stable" ]]; then
 elif [[ "${release_type}" == "alpha" ]]; then
   export pkgname="mpm-alpha"
 fi
-cat src/PKGBUILD | envsubst '$pkgname $release_type $FUNCTIONS_DIR $REPO_DIR'
+cat src/PKGBUILD | envsubst '$pkgname $release_type $FUNCTIONS_DIR $REPO_DIR' | tee src/PKGBUILD
+cat src/PKGBUILD && exit 1
 
 # Set up repository and install makedeb
 echo "+ Setting up repository"
