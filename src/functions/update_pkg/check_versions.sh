5a00f22acbdf87c75555c21c45600c832566a0d7
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-05-03 02:01:53 -0500

    Fixed issue with version check in update_pkg()

--- src/functions/update_pkg/check_versions.sh
+++ src/functions/update_pkg/check_versions.sh
@@ -7,7 +7,7 @@ check_versions() {
     filever=$(echo ${file} | awk -F"_" '{print $2}')
     aurver=$(curl -s "${CHECK_URL}" | jq -r '.results[].Version')
 
-    higher_ver=$(echo ${filever} ${aurver} | sed 's/ /\n/g' | sort -V | xargs | awk '{print $1}')
+    higher_ver=$(echo ${filever} ${aurver} | sed 's/ /\n/g' | sort -V | xargs | awk '{print $2}')
 
     apt list ${filename} 2> /dev/null | grep "\[installed" &> /dev/null
     if [[ ${?} != "0" ]]; then
