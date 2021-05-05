4b1700aefede0e3e123599ea3e60d0c57bfd63c0
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-05-05 17:23:59 -0500

    Changed to make apt install packages on local repository; Other changes
    - Set to remove mpm.list file after updating so ignore messages don't 
    spam the terminal when a user runs 'apt update'

--- src/functions/install_pkg/install_packages.sh
+++ src/functions/install_pkg/install_packages.sh
@@ -1,9 +1,21 @@
 install_packages() {
 
-  echo "Installing '${PKG}'..."
+  echo "Installing packages..."
+  sudo echo "deb [trusted=yes] file:///etc/mpm/repo /" > /etc/apt/sources.list.d/mpm.list
+
   cd /etc/mpm/repo
   dpkg-scanpackages debs | sudo tee Packages &> /dev/null
-  sudo apt-get update -o Dir::Etc::sourcelist="sources.list.d/mpm.list" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0" &> /dev/null
-  sudo apt install ${PKG} -y
+  sudo apt-get update -o Dir::Etc::sourcelist="sources.list.d/mpm.list" \
+                      -o Dir::Etc::sourceparts="-" \
+                      -o APT::Get::List-Cleanup="0" &> /dev/null
 
+  sudo apt install -y -o Dir::Etc::sourcelist="sources.list.d/mpm.list" \
+                      -o Dir::Etc::sourceparts="-" \
+                      -o APT::Get::List-Cleanup="0" \
+                      ${PKG} -y
+
+  sudo rm /etc/apt/sources.list.d/mpm.list
+  sudo apt-get update -o Dir::Etc::sourcelist="sources.list.d/mpm.list" \
+                      -o Dir::Etc::sourceparts="-" \
+                      -o APT::Get::List-Cleanup="0" &> /dev/null
 }
