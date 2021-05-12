350a13bdd0ca0acc08d7ed8f011f81321bade7a8
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-05-11 23:32:42 -0500

    Fix repo issue when deb file for AUR package didn't exist locally

--- src/functions/install_pkg/build_packages.sh
+++ src/functions/install_pkg/build_packages.sh
@@ -4,7 +4,7 @@ aur_build_packages() {
     makedeb
     CONTROL_NAME="$(cat pkg/DEBIAN/control | grep "Package:" | awk '{print $2}')_$(cat pkg/DEBIAN/control | grep "Version:" | awk '{print $2}')_$(cat pkg/DEBIAN/control | grep "Architecture:" | awk '{print $2}')"
 
-    sudo rm /etc/mpm/repo/debs/"$(cat pkg/DEBIAN/control | grep "Package:" | awk '{print $2}')"* &> /dev/null
+    { sudo rm /etc/mpm/repo/debs/"$(cat pkg/DEBIAN/control | grep "Package:" | awk '{print $2}')"* &> /dev/null; } || printf ""
     sudo cp ${CONTROL_NAME}.deb /etc/mpm/repo/debs
 
     export_package_db=$(echo ${i}\\$(cat pkg/DEBIAN/control | grep "Version:" | awk '{print $2}')\\aur)
