5f4e4d7d6eb8743c72f0f0b72b3f4619e7898dbb
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-05-13 20:35:34 -0500

    Added --convert option to makedeb commands in build_packages.sh

--- src/functions/install_pkg/build_packages.sh
+++ src/functions/install_pkg/build_packages.sh
@@ -1,7 +1,7 @@
 aur_build_packages() {
   for i in ${aur_packages}; do
     cd "${i}"
-    makedeb
+    makedeb --convert
     CONTROL_NAME="$(cat pkg/DEBIAN/control | grep "Package:" | awk '{print $2}')_$(cat pkg/DEBIAN/control | grep "Version:" | awk '{print $2}')_$(cat pkg/DEBIAN/control | grep "Architecture:" | awk '{print $2}')"
 
     { sudo rm /etc/mpm/repo/debs/"$(cat pkg/DEBIAN/control | grep "Package:" | awk '{print $2}')"* &> /dev/null; } || printf ""
@@ -21,7 +21,7 @@ aur_build_packages() {
 arch_repository_build_packages() {
   for i in ${arch_repository_packages}; do
     cd "${i}"
-    makedeb --prebuilt
+    makedeb --prebuilt --convert
     CONTROL_NAME="$(cat pkg/DEBIAN/control | grep "Package:" | awk '{print $2}')_$(cat pkg/DEBIAN/control | grep "Version:" | awk '{print $2}')_$(cat pkg/DEBIAN/control | grep "Architecture:" | awk '{print $2}')"
     { sudo rm /etc/mpm/repo/debs/"$(cat pkg/DEBIAN/control | grep "Package:" | awk '{print $2}')"* &> /dev/null; } || printf ""
     sudo cp ${CONTROL_NAME}.deb /etc/mpm/repo/debs
