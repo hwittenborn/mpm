f9795de1b10da94e8ca497592a716bab34f77353
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-05-18 21:00:34 -0500

    Fixed issue when spacing is corrected in sources.db

--- src/functions/update_pkg/clean_sources_db.sh
+++ src/functions/update_pkg/clean_sources_db.sh
@@ -3,11 +3,16 @@ clean_sources_db() {
 
   local action1=$(echo "${sources_db}" | sed 's|\\|\\\\|g' | sed 's|$|;|g' | xargs | sed 's| ||g')
 
-  while echo "${action1}" | grep ';;'; do
+  while echo "${action1}" | grep ';;' &> /dev/null; do
     action1=$(echo "${action1}" | sed 's|;;|;|g')
   done
 
-  new_action1=$(echo "${action1}" | sed 's|;||' | rev | sed 's|;||' | rev | sed 's|;|\n|g')
+  if [[ $(echo "${action1}" | head -c1) = ";" ]]; then
+    new_action1=$(echo "${action1}" | sed "s|;||" | sed 's|;|\n|g')
+  else
+    new_action1=$(echo "${action1}" | sed 's|;|\n|g')
+  fi
+
 
   echo "${new_action1}" | sudo tee /etc/mpm/sources.db &> /dev/null
 }
