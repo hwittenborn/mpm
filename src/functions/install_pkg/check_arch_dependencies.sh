5d3bb2077cd24b144b56e9674efa63d4267578f1
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-05-20 19:06:47 -0500

    Fixed issue with Arch repository dependency checks

--- src/functions/install_pkg/check_arch_dependencies.sh
+++ src/functions/install_pkg/check_arch_dependencies.sh
@@ -7,22 +7,21 @@ check_arch_dependencies() {
     fi
   done
 
-  add_arch_dependency_cache=${add_arch_dependency_list}
-
   # Keep looping until cache is equal to packages added on run;
   # used for recursive dependencies.
   while true; do
+    local grep_string=$(echo "${add_arch_dependency_cache}" | sed 's/ /|/g')
     for i in ${add_arch_dependency_list}; do
       local arch_dependency_results=$(makedeb-db --package ${i} | jq -r .arch_packages | grep -vw "null")
 
-      if ! { echo "${PKG}" | grep "${arch_dependency_results}" &> /dev/null; }; then
+      if ! { echo "${PKG}" | grep -vE "${grep_string}" &> /dev/null; }; then
         add_arch_dependency_list+=" ${arch_dependency_results}"
       fi
 
       if [[ $(echo ${add_arch_dependency_cache} | xargs) != $(echo ${add_arch_dependency_list} | xargs) ]]; then
         add_arch_dependency_cache=${add_arch_dependency_list}
       else
-        continue
+        break 2
       fi
     done
 
