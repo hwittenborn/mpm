5604cf1707c8ca51acc521db173e740d7b3c73ce
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-06-01 16:07:40 -0500

    Modified spacing, indentation, and comments to make more legible

--- src/functions/update_pkg/check_versions.sh
+++ src/functions/update_pkg/check_versions.sh
@@ -1,36 +1,61 @@
 check_versions() {
-  local sources_db_content=$(cat /etc/mpm/sources.db)
-  local number="1"
-  for i in $(echo "${sources_db_content}" | awk -F '\' '{print $1}'); do
-    if [[ $(echo "${sources_db_content}" | awk -F '\' '{print $3}' | awk NR==${number}) == "aur" ]]; then
-      local pkg_local_version=$(echo "${sources_db_content}" | awk -F '\' '{print $2}' | awk NR==${number})
-      local pkg_aur_version=$(curl -s "${aur_url}rpc.php/rpc/?v=5&type=info&arg=${i}" | jq .results[].Version)
+    # Read database, and set 'number' used for awk to read each line
+    local sources_db_content=$(cat /etc/mpm/sources.db)
+    local number="1"
 
-      local pkg_highest_version=$(echo ${pkg_local_version} ${pkg_aur_version} | sort -V | awk '{print $2}')
+    # Start loop of each package in database
+    for i in $(echo "${sources_db_content}" | awk -F '\' '{print $1}'); do
 
-      if [[ ${pkg_highest_version} != ${pkg_local_version} ]]; then
-        to_update+=" ${i}"
-      fi
+        # Check if package is from AUR
+        if [[ $(echo "${sources_db_content}" | awk -F '\' '{print $3}' | awk NR==${number}) == "aur" ]]; then
 
-    elif [[ $(echo "${sources_db_content}" | awk -F '\' '{print $3}' | awk NR==${number}) == "arch_repository" ]]; then
-      local pkg_local_version=$(echo "${sources_db_content}" | awk -F '\' '{print $2}' | awk NR==${number})
-      local pkg_arch_repository_results=$(curl -s "${arch_repository_search_url}${i}")
-        local pkg_arch_repository_epoch=$(echo ${pkg_arch_repository_results} | jq .results[].epoch)
-        local pkg_arch_repository_epoch=$(echo ${pkg_arch_repository_results} | jq .results[].pkgver)
-        local pkg_arch_repository_epoch=$(echo ${pkg_arch_repository_results} | jq .results[].pkgrel)
-        if [[ "${epoch}" -gt "1" ]]; then
-          pkg_arch_repository_epoch_status="${epoch}:"
+            # Check local version of package
+            local pkg_local_version=$(echo "${sources_db_content}" | awk -F '\' '{print $2}' | awk NR==${number})
+
+            # Check version of package in AUR
+            local pkg_aur_version=$(curl -s "${aur_url}rpc.php/rpc/?v=5&type=info&arg=${i}" | jq .results[].Version)
+
+            # Check which version is highest
+            local pkg_highest_version=$(echo ${pkg_local_version} ${pkg_aur_version} | sort -V | awk '{print $2}')
+
+            if [[ ${pkg_highest_version} != ${pkg_local_version} ]]; then
+                to_update+=" ${i}"
+            fi
+
+        # Check if package is from the Arch Linux repositories
+        elif [[ $(echo "${sources_db_content}" | awk -F '\' '{print $3}' | awk NR==${number}) == "arch_repository" ]]; then
+
+            # Check local version of package
+            local pkg_local_version=$(echo "${sources_db_content}" | awk -F '\' '{print $2}' | awk NR==${number})
+
+            # Check version of package in Arch Linux repositories
+            local pkg_arch_repository_results=$(curl -s "${arch_repository_search_url}${i}")
+
+                # Get epoch, pkgver, and pkgrel
+                local pkg_arch_repository_epoch=$(echo ${pkg_arch_repository_results} | jq .results[].epoch)
+                local pkg_arch_repository_pkgver=$(echo ${pkg_arch_repository_results} | jq .results[].pkgver)
+                local pkg_arch_repository_pkgrel=$(echo ${pkg_arch_repository_results} | jq .results[].pkgrel)
+
+                # Add epoch if greater than 1
+                if [[ "${epoch}" -gt "1" ]]; then
+                    pkg_arch_repository_epoch_status="${epoch}:"
+                fi
+
+                # Set Arch Linux repository version
+                pkg_arch_repository_version="${pkg_arch_repository_epoch_status}${pkgver}-${pkgrel}"
+
+            # Check which version is highest
+            local pkg_highest_version=$(echo ${pkg_local_version} ${pkg_arch_repository_version} | sort -V | awk '{print $2}')
+
+            if [[ ${pkg_highest_version} != ${pkg_local_version} ]]; then
+                to_update+=" ${i}"
+            fi
         fi
-        pkg_arch_repository_version="${pkg_arch_repository_epoch_status}${pkgver}-${pkgrel}"
 
-        local pkg_highest_version=$(echo ${pkg_local_version} ${pkg_arch_repository_version} | sort -V | awk '{print $2}')
+        # Restart loop with $number set to one higher
+        number=$(( ${number} + 1 ))
+    done
 
-        if [[ ${pkg_highest_version} != ${pkg_local_version} ]]; then
-          to_update+=" ${i}"
-        fi
-    fi
-
-    number=$(( ${number} + 1 ))
-  done
-  to_update=$(echo ${to_update} | xargs)
+    # Set packages that need to be updated, then run 'xargs' to fix spacing
+    to_update=$(echo ${to_update} | xargs)
 }
--- src/functions/update_pkg/remove_packages.sh
+++ src/functions/update_pkg/remove_packages.sh
@@ -12,13 +12,18 @@ remove_packages() {
   done
 
   local sources_db_packages=$(cat /etc/mpm/sources.db | awk -F '\' '{print $1}' | xargs)
+
   # Configure output for processing by 'grep'
   local new_sources_db_packages=$(echo "${sources_db_packages}" | sed 's| |\||g')
   local unneeded_packages=$(ls /etc/mpm/repo/debs | sed 's| |\n|g' | grep -Ev "${new_sources_db_packages}" | xargs)
+
   if [[ "${unneeded_packages}" != "" ]]; then
+
     echo "Removing unneded packages from local repository..."
+
     for i in ${unneeded_packages}; do
       sudo rm -f /etc/mpm/repo/debs/"${i}"*
     done
+    
   fi
 }
