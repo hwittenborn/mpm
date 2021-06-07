26a209abf0c1176548c694269c3a047f949238df
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-06-07 18:58:22 -0500

    Redid aur_check_files() to align with '[Y/n]' prompt
    Resulted in removal of bug in 'read' command

--- src/functions/install_pkg/aur_check_files.sh
+++ src/functions/install_pkg/aur_check_files.sh
@@ -1,40 +1,27 @@
 aur_check_files() {
-  for i in ${aur_packages}; do
-    echo "Look over files for '${i}'?"
-    printf "Enter yes(y) or no(n): "
-    read check_files
+    for i in ${aur_packages}; do
+        read -p "Look over files for '${i}'? [Y/n]" check_files
 
-    while [[ ${check_files} != "yes" ]] && [[ ${check_files} != "y" ]] && [[ ${check_files} != "no" ]] && [[ ${check_files} != "n" ]]; do
-      echo "Invalid option provided"
-      printf " Enter yes(y) or no(n): "
-      read -p check_files
+        while [[ ${check_files,,} != "n" ]]; do
+            nano "${i}"/PKGBUILD
+            source "${i}"/PKGBUILD
+
+            num="0"
+            while [[ $(eval echo \${source[${num}]}) != "" ]]; do
+
+                if eval echo \${source[${num}]} | grep "http" &> /dev/null; then
+                    num=$(( ${num} + 1 ))
+                    continue
+                fi
+
+                nano "${i}"/$(eval echo \${source[${num}]} | sed 's|[^ ]*/||')
+                num=$(( ${num} + 1 ))
+
+            done
+            sleep 1
+
+            read -p "Look over files for '${i}'? [Y/n]" check_files
+        done
+        echo
     done
-
-    while [[ ${check_files} == "yes" ]] || [[ ${check_files} == "y" ]]; do
-      nano "${i}"/PKGBUILD
-      source "${i}"/PKGBUILD
-
-      num="0"
-      while [[ $(eval echo \${source[${num}]}) != "" ]]; do
-        if eval echo \${source[${num}]} | grep "http" &> /dev/null; then
-          num=$(( ${num} + 1 ))
-          continue
-        fi
-
-        nano "${i}"/$(eval echo \${source[${num}]} | sed 's|[^ ]*/||')
-        num=$(( ${num} + 1 ))
-      done
-      sleep 1
-
-      echo "Look over files for '${i}'?"
-      printf "Enter yes(y) or no(n): "
-      read check_files
-
-      while [[ ${check_files} != "yes" ]] && [[ ${check_files} != "y" ]] && [[ ${check_files} != "no" ]] && [[ ${check_files} != "n" ]]; do
-        echo "Invalid option provided"
-        printf "Enter yes(y) or no(n): "
-        read check_files
-      done
-    done
-  done
 }
