30a049d0db0563a0244d4f839d542929884219ef
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-06-07 19:32:59 -0500

    Fixed issue with mpm.list removal during cleanup

--- src/functions/install_pkg/install_packages.sh
+++ src/functions/install_pkg/install_packages.sh
@@ -5,10 +5,6 @@ install_packages() {
                       -o Dir::Etc::sourceparts="-" \
                       -o APT::Get::List-Cleanup="0" &> /dev/null
 
-  sudo apt "${OP}" $([[ "${OP}" != "upgrade" ]] && echo ${aur_packages} ${arch_repository_packages}) -y
 
-  sudo rm /etc/apt/sources.list.d/mpm.list
-  sudo apt-get update -o Dir::Etc::sourcelist="sources.list.d/mpm.list" \
-                      -o Dir::Etc::sourceparts="-" \
-                      -o APT::Get::List-Cleanup="0" &> /dev/null
+  sudo apt "${OP}" $([[ "${OP}" != "upgrade" ]] && echo ${aur_packages} ${arch_repository_packages}) -y
 }
