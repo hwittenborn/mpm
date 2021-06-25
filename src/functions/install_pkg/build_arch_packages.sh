847730595daf74e8d7ae8f5fa9d1de9c24888033
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-06-25 16:50:49 -0500

    Purged mpm

--- src/functions/install_pkg/build_arch_packages.sh
+++
@@ -1,5 +0,0 @@
-build_arch_packages() {
-  for i in ${arch_repository_packages}; do
-      build_package "${i}" "arch_repository"
-  done
-}
--- src/functions/install_pkg/build_aur_packages.sh
+++
@@ -1,5 +0,0 @@
-build_aur_packages() {
-  for i in ${aur_packages}; do
-      build_package "${i}" "aur"
-  done
-}
