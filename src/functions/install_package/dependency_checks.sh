e50295cebaa45100d8011b45881ba529c0780c46
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-07-08 20:24:10 -0500

    Fixed package installation when no dependencies are present

--- src/functions/install_package/dependency_checks.sh
+++ src/functions/install_package/dependency_checks.sh
@@ -15,30 +15,32 @@ dependency_checks() {
     dependency_packages="$(echo "${dependencies_temp}" "${make_dependencies_temp}" "${check_dependencies_temp}" | sed 's| |\n|g' | sort -u | xargs)"
     unset dependencies_temp make_dependencies_temp check_dependencies_temp
 
-    for i in ${dependency_packages}; do
-        generate_package_parenthesis "${i}"
-    done
+	if [[ "${dependency_packages}" != "" ]]; then
 
-    apt_output="$(eval apt-get satisfy -sq ${apt_packages[@]})"
+		for i in ${dependency_packages}; do
+        	generate_package_parenthesis "${i}"
+    	done
 
-    apt_bad_packages="$(echo "${apt_output}" | grep -o '[^ ]* but it is not installable' | sed 's| but it is not installable||')"
+    	apt_output="$(eval apt-get satisfy -sq ${apt_packages[@]})"
 
-    if [[ "${apt_bad_packages}" != "" ]]; then
-        echo "The following build dependencies are unable to be installed:"
-        echo "  ${apt_bad_packages}"
-        exit 1
+    	apt_bad_packages="$(echo "${apt_output}" | grep -o '[^ ]* but it is not installable' | sed 's| but it is not installable||')"
+
+    	if [[ "${apt_bad_packages}" != "" ]]; then
+        	echo "The following build dependencies are unable to be installed:"
+        	echo "  ${apt_bad_packages}"
+        	exit 1
+    	fi
+
+    	unset apt_bad_packages
+
+    	# This gets used back in install_package() to list both build dependencies
+    	# *and* packages to be built
+    	apt_needed_dependencies="$(echo "${apt_output}" |
+                                   sed 's|$| |g' |
+                                   tr -d '\n' |
+                                   grep -o 'The following NEW packages will be installed:.*[[:digit:]] upgraded' |
+                                   sed 's|The following NEW packages will be installed:||' |
+                                   sed 's|[[:digit:]] upgraded||' |
+                                   xargs)" || true
     fi
-
-    unset apt_bad_packages
-
-    # This gets used back in install_package() to list both build dependencies
-    # *and* packages to be built
-    apt_needed_dependencies="$(echo "${apt_output}" |
-                                sed 's|$| |g' |
-                                tr -d '\n' |
-                                grep -o 'The following NEW packages will be installed:.*[[:digit:]] upgraded' |
-                                sed 's|The following NEW packages will be installed:||' |
-                                sed 's|[[:digit:]] upgraded||' |
-                                xargs)" || true
-
 }
